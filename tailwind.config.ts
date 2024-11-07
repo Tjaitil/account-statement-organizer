a8cf28eb9a4a8429a7b6b170e37c2d2be09077f2
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-11-07 20:37:26 +0100

    feat: add tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss/types/config';
+
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    './index.html',
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+} satisfies Config;
