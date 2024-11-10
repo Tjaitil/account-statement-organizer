0be51f4e42e689c725930e2157bd774a9946e964
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2024-11-10 16:39:33 +0100

    refactor: use default tailwind instead of css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
+<template>
+    <section class="bg-slate-100 w-screen h-screen pt-12 flex place-content-center">
+        <div class="max-w-6xl">
+        </div>
+    </section>
+</template>
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
