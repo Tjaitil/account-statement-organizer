2acb58c64a4787493b0afff7acc5667141475ad1
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-04-07 19:20:20 +0200

    chore: add test files

---
+++ src/assets/demoFile.ts
@@ -0,0 +1,7 @@
+
+import demoFileUrl from "./demo-file.csv?url";
+export const demoFile = new File(
+    [await fetch(demoFileUrl).then(res => res.text())],
+    'demo-file.csv',
+    { type: 'text/csv' }
+);
